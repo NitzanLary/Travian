d0c032d3faf7bfa0163fc96676a51d0d080809b2
LaryNitzan <nitzan.lary@intel.com>
2025-08-28 23:35:43 +0300

    correct url

--- sql2csv.py
+++ sql2csv.py
@@ -64,7 +64,7 @@ def write_csv(records: list[list[str]], headers: list[str], output_path: str) ->
 
 
 def main():
-    URL = "https://ts21.x2.international.travian.com/map.sql"
+    URL = "https://ts50.x5.america.travian.com/map.sql"
     output_file = "map.csv"
     from_file = False # Change to False to fetch from URL
 
